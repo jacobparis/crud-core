c6a3e2bf88de371988a21d6020958a026d85d703
Jacob Paris <jacob@jacobparis.com>
2024-09-16 02:55:01 +0800

    Update from upstream

---
+++ exercises/01.create/01.solution.nested-routing/exercises/01.create/01.solution.nested-routing/app/routes/_seo+/sitemap[.]xml.ts
@@ -0,0 +1,13 @@
+import { generateSitemap } from '@nasa-gcn/remix-seo'
+import { type ServerBuild, type LoaderFunctionArgs } from '@remix-run/node'
+import { getDomainUrl } from '#app/utils/misc.tsx'
+
+export async function loader({ request, context }: LoaderFunctionArgs) {
+	const serverBuild = (await context.serverBuild) as { build: ServerBuild }
+	return generateSitemap(request, serverBuild.build.routes, {
+		siteUrl: getDomainUrl(request),
+		headers: {
+			'Cache-Control': `public, max-age=${60 * 5}`,
+		},
+	})
+}
